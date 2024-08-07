183ce8a02f0b416a45eab1707f6cb14feeaf8b5f
shubhamcypher <shubhamdevo23@gmail.com>
2024-08-07 06:25:43 +0530

    all things removed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,8 +4,8 @@ import { Outlet } from 'react-router-dom';
 
 const Home = () => {
   return (
-    <div className='flex h-full gap-4'>
-        <div className=' w-1/8 border-2 border-black rounded-xl p-4 flex flex-col justify-between lg:block sm:hidden'><Sidebar/></div>
+    <div className='flex h-[98vh] gap-4'>
+        <div className=' w-1/8 border-2 border-black rounded-xl p-4 flex flex-col justify-between'><Sidebar/></div>
         <div className='w-5/6 border-2 border-black rounded-xl p-4'>
           <Outlet/>
         </div>
