c01852d2dada31412a6f67be4a5410bbe70ec583
shubhamcypher <shubhamdevo23@gmail.com>
2024-08-07 20:38:17 +0530

    all set

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -41,8 +41,8 @@ const Login = () => {
     }
   }
   return (
-    <div className=' h-[98vh] bg-black flex items-center justify-center' style={{ backgroundImage: 'url(https://4kwallpapers.com/images/walls/thumbs_3t/17760.png)' }}>
-        <div className='p-4 lg:w-2/6 rounded bg-gray-700'>
+    <div className=' h-[98vh] bg-black flex items-center justify-center' style={{ backgroundImage: 'url(/login.png)' }}>
+        <div className='p-4 lg:w-2/6 rounded bg-gray-900'>
             <div className='font-bold text-2xl mb-2'>Log in</div>
             <input type='text' name='username' placeholder='username' value={data.username} onChange={handleChange} className='w-full px-3 py-2 my-3 text-gray-200 rounded bg-gray-600'/>
             <input type='password' name='password' placeholder='password' value={data.password} onChange={handleChange} className='w-full px-3 py-2 my-3 text-gray-200 rounded bg-gray-600'/>
